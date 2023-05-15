22dfbce228795e316a749e1ccc8e8adc4f7a3f15
JosephGakah <joegakah@gmail.com>
2023-05-15 16:30:59 +0300

    initialization.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,11 @@
 import { Footer, Header } from '../components'
 import AppRouter from '../router/router'
+import ReactGA from 'react-ga';
+
+
+const TRACKING_ID = "UA-XXXXX-X"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 
 function App() {
   return (
